2f1abda99077f5341e9e49b44893416fa11d0597
Stamp9 <383990832@qq.com>
2024-11-16 20:45:45 +0700

    feat: checking node connection when testing.

--- hardpint/test/counter.test.js
+++ hardpint/test/counter.test.js
@@ -1,10 +1,49 @@
 const { expect } = require('chai');
 const path = require('path');
+const { execSync } = require('child_process');
 const { deployContract, queryState, submitSolution } = require('./helpers');
+const findProcess = require('find-process');
+const { spawn } = require('child_process');
+
 
 describe('Counter Contract', () => {
   const sourcePath = path.join(__dirname, '../contracts');
   const contractName = 'counter';
+
+  before(async () => {
+    try {
+      execSync('nc -zv 0.0.0.0 3554', { stdio: 'inherit' });
+      console.log('^.^ Node is running...');
+    } catch (error) {
+      console.error('Node is not running: ', error.message);
+      console.log('^.^ Starting node...');
+      // execSync('npx hardhat node');
+      hardhatProcess = spawn('npx', ['hardhat', 'node'], {
+        detached: true,
+        stdio: 'ignore'
+      });
+      try {
+        await new Promise(resolve => setTimeout(resolve, 5000));
+      } catch (error) {
+        console.error('Error during node startup:', error.message);
+        throw error;
+      }
+    }
+  });
+
+  after(async () => {
+    // Kill the Hardhat node process
+    try {
+      const node_list = await findProcess('port', 3553);
+      // const builder_list = await findProcess('port', 3554);
+      if (node_list.length) {
+        execSync(`kill -9 ${node_list[0].pid}`, { stdio: 'ignore' });
+        console.log('Hardhat node stopped >.<');
+      }
+    } catch (error) {
+      console.error('Error stopping Hardhat node:', error.message);
+    }
+  });
 
   it('should deploy successfully', async () => {
     const contractInfo = await deployContract(contractName, sourcePath);
@@ -15,7 +54,7 @@ describe('Counter Contract', () => {
     const state = await queryState(contractInfo.contractAddress, "0000000000000000");
     expect(state).to.be.equal('null');
 
-    const mutations = [{key: [0], value: [1]}];
+    const mutations = [{ key: [0], value: [1] }];
 
     const solution = await submitSolution(contractInfo.contractAddress, contractInfo.methodAddress, [], mutations);
     expect(solution).to.be.equal("68A1BC8E7A5E6789E8DE4BC59A7ADD0BFC5AF7FA591FCC03CEBE6E4754C13CA1");
--- hardpint/test/helpers.js
+++ hardpint/test/helpers.js
@@ -9,12 +9,12 @@ const fs = require('fs');
  */
 async function compilePintContracts(sourcePath) {
   console.log("Compiling pint smart contracts...");
-try{
-    const output = execSync('pint build', { 
+  try {
+    const output = execSync('pint build', {
       encoding: 'utf-8',
       cwd: sourcePath
     });
-    
+
     console.log(output);
     let l = output.split('\n');
     let s = l[2].split(' ');
@@ -43,75 +43,75 @@ try{
  * @returns {Promise<object>} Object containing contract and method addresses
  */
 async function deployContract(contractName, sourcePath, url = "http://127.0.0.1:3554") {
-    console.log(`Deploying contract: ${contractName}...`);
-    
-    const contractsDir = path.join(sourcePath, 'out/debug');
-    const contractFile = `${contractName}.json`;
-    const contractPath = path.join(contractsDir, contractFile);
-    
-    const contractInfo = await compilePintContracts(sourcePath);
-    if (!contractInfo.contractAddress || !fs.existsSync(contractPath)) {
-        throw new Error(`Contract file ${contractFile} not found in ${contractsDir} even after compilation`);
-    }
-  
-    try {
-      console.log(`Using network: ${url}`);
-      const cmd = `essential-rest-client deploy-contract "${url}" "${contractPath}"`;
-      const output = execSync(cmd, { encoding: 'utf-8' });
-      console.log('Deployment output:', output);
-      let l = output.split('\n')[0];
-      
-      return {contractAddress: contractInfo.contractAddress, methodAddress: contractInfo.methodAddress, address: l};
-      
-    } catch (error) {
-      throw new Error(`Error deploying contract ${contractName}: ${error.message}`);
-    }
+  console.log(`Deploying contract: ${contractName}...`);
+
+  const contractsDir = path.join(sourcePath, 'out/debug');
+  const contractFile = `${contractName}.json`;
+  const contractPath = path.join(contractsDir, contractFile);
+
+  const contractInfo = await compilePintContracts(sourcePath);
+  if (!contractInfo.contractAddress || !fs.existsSync(contractPath)) {
+    throw new Error(`Contract file ${contractFile} not found in ${contractsDir} even after compilation`);
   }
 
-  async function queryState(contentAddress, key, url = "http://127.0.0.1:3553") {
-    console.log(`Querying state of contract: ${contentAddress}...`);
-    try {
-      const cmd = `essential-rest-client query-state --content-address "${contentAddress}" "${url}" "${key}"`;
-      const output = execSync(cmd, { encoding: 'utf-8' });
-      console.log('Query state output:', output);
-      return output.split('\n')[0];
-    } catch (error) {
-      throw new Error(`Error querying state of contract ${contentAddress}: ${error.message}`);
-    }
-  }
+  try {
+    console.log(`Using network: ${url}`);
+    const cmd = `essential-rest-client deploy-contract "${url}" "${contractPath}"`;
+    const output = execSync(cmd, { encoding: 'utf-8' });
+    console.log('Deployment output:', output);
+    let l = output.split('\n')[0];
 
-  async function submitSolution(contractAddress, methodAddress, decisionVars, stateMutations, url = "http://127.0.0.1:3554") {
-    console.log(`Submitting solution to contract: ${contractAddress}...`);
-    try {
-      // essential-rest-client submit-solution "http://127.0.0.1:3554" ./solution.json
-      // create solution.json
-      const predicate = {
-        contract: contractAddress,
-        predicate: methodAddress
-      };
-      const entry = {
-        predicate_to_solve: predicate,
-        decision_variables: decisionVars,
-        state_mutations: stateMutations
-      };
+    return { contractAddress: contractInfo.contractAddress, methodAddress: contractInfo.methodAddress, address: l };
 
-      const solution = {
-        data: [entry]
-      }
-      const solutionPath = path.join(__dirname, `./solution.json`);
-      fs.writeFileSync(solutionPath, JSON.stringify(solution, null, 2));
-      const cmd = `essential-rest-client submit-solution "${url}" ${solutionPath}`;
-      const output = execSync(cmd, { encoding: 'utf-8' });
-      console.log('Submit solution output:', output);
-      return output.split('\n')[0];
-    } catch (error) {
-      throw new Error(`Error submitting solution to contract ${contractAddress}: ${error.message}`);
-    }
+  } catch (error) {
+    throw new Error(`Error deploying contract ${contractName}: ${error.message}`);
   }
-  
-  module.exports = {
-      compilePintContracts,
-      deployContract,
-      queryState,
-      submitSolution
-  };
+}
+
+async function queryState(contentAddress, key, url = "http://127.0.0.1:3553") {
+  console.log(`Querying state of contract: ${contentAddress}...`);
+  try {
+    const cmd = `essential-rest-client query-state --content-address "${contentAddress}" "${url}" "${key}"`;
+    const output = execSync(cmd, { encoding: 'utf-8' });
+    console.log('Query state output:', output);
+    return output.split('\n')[0];
+  } catch (error) {
+    throw new Error(`Error querying state of contract ${contentAddress}: ${error.message}`);
+  }
+}
+
+async function submitSolution(contractAddress, methodAddress, decisionVars, stateMutations, url = "http://127.0.0.1:3554") {
+  console.log(`Submitting solution to contract: ${contractAddress}...`);
+  try {
+    // essential-rest-client submit-solution "http://127.0.0.1:3554" ./solution.json
+    // create solution.json
+    const predicate = {
+      contract: contractAddress,
+      predicate: methodAddress
+    };
+    const entry = {
+      predicate_to_solve: predicate,
+      decision_variables: decisionVars,
+      state_mutations: stateMutations
+    };
+
+    const solution = {
+      data: [entry]
+    }
+    const solutionPath = path.join(__dirname, `./solution.json`);
+    fs.writeFileSync(solutionPath, JSON.stringify(solution, null, 2));
+    const cmd = `essential-rest-client submit-solution "${url}" ${solutionPath}`;
+    const output = execSync(cmd, { encoding: 'utf-8' });
+    console.log('Submit solution output:', output);
+    return output.split('\n')[0];
+  } catch (error) {
+    throw new Error(`Error submitting solution to contract ${contractAddress}: ${error.message}`);
+  }
+}
+
+module.exports = {
+  compilePintContracts,
+  deployContract,
+  queryState,
+  submitSolution
+};
