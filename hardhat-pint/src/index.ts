7019756998f8eab5ba52579f55e6f15ec433265f
Stamp9 <383990832@qq.com>
2024-11-17 01:13:54 +0700

    hotfix

--- hardhat-pint/src/index.ts
+++ hardhat-pint/src/index.ts
@@ -2,9 +2,11 @@ import { extendConfig, extendEnvironment, task } from "hardhat/config";
 import { lazyObject } from "hardhat/plugins";
 import { HardhatConfig, HardhatUserConfig } from "hardhat/types";
 import { Pint } from "./Pint"
+
 // This import is needed to let the TypeScript compiler know that it should include your type
 // extensions in your npm package's types file.
 import "./type-extensions";
+
 
 extendConfig(
   (config: HardhatConfig, userConfig: Readonly<HardhatUserConfig>) => {
